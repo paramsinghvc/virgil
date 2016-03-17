a4aa7d63513167eb765e2eee6fd4aa3326e96fb4
Parminder Singh <paramsinghvc@gmail.com>
2016-03-17 17:22:22 +0530

    chartjs

--- app/src/directives/chartDirective.js
+++ app/src/directives/chartDirective.js
@@ -16,34 +16,38 @@ export default class ChartDirective {
 
 	compile($el) {
 		return ($scope, $el, $attrs) => {
-			this.$timeout(() => {
-				let ctx = $el[0].getContext('2d');
-				let chart = new Chart(ctx);
-				let chartObj;
-				switch ($scope.chartType) {
-					case 'line':
-						chartObj = chart.Line($scope.chartData, {
-							responsive: true
-						});
-						break;
-					case 'bar':
-						chartObj = chart.Bar($scope.chartData, $scope.chartOptions);
-						break;
-					case 'pie':
-						chartObj = chart.Pie($scope.chartData, $scope.chartOptions);
-						break;
-					case 'polar':
-						chartObj = chart.PolarArea($scope.chartData, $scope.chartOptions);
-						break;
-					case 'radar':
-						chartObj = chart.Radar($scope.chartData, $scope.chartOptions);
-						break;
-					case 'doughnut':
-						chartObj = chart.Doughnut($scope.chartData, $scope.chartOptions);
-						break;
-				}
+			$scope.$watch('chartData', (n, o) => {
+				if (n) {
+					this.$timeout(() => {
+						let ctx = $el[0].getContext('2d');
+						let chart = new Chart(ctx);
+						let chartObj;
+						switch ($scope.chartType) {
+							case 'line':
+								chartObj = chart.Line($scope.chartData, {
+									responsive: true
+								});
+								break;
+							case 'bar':
+								chartObj = chart.Bar($scope.chartData, $scope.chartOptions);
+								break;
+							case 'pie':
+								chartObj = chart.Pie($scope.chartData, $scope.chartOptions);
+								break;
+							case 'polar':
+								chartObj = chart.PolarArea($scope.chartData, $scope.chartOptions);
+								break;
+							case 'radar':
+								chartObj = chart.Radar($scope.chartData, $scope.chartOptions);
+								break;
+							case 'doughnut':
+								chartObj = chart.Doughnut($scope.chartData, $scope.chartOptions);
+								break;
+						}
 
-			});
+					});
+				}
+			})
 		}
 	}
 
